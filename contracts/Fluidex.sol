5edc271906be23004b3a33b70e7471c27538f4f3
HAOYUatHZ <haoyu@protonmail.com>
2021-06-28 22:10:54 +0800

    merge

--- contracts/Fluidex.sol
+++ contracts/Fluidex.sol
@@ -9,9 +9,34 @@ contract FluidexDemo {
       Verified
    }
 
+   uint256 GENESIS_ROOT;
+   mapping(uint256 => uint256) public state_roots;   
    mapping(uint256 => BlockState) public block_states;
 
-   function get_block_state_by_block_id(uint256 block_id) public returns (BlockState) {
-      return block_states[block_id];
+   constructor(uint256 _genesis_root) public {
+      GENESIS_ROOT = _genesis_root;
+   }
+
+   function get_block_state_by_block_id(uint256 _block_id) public returns (BlockState) {
+      return block_states[_block_id];
+   }
+
+   function submit_block(
+      uint256 _block_id,
+      uint256[] memory _public_inputs,
+      uint256[] memory _serialized_proof
+   ) public returns (bool) {
+      // _public_inputs[0] is previous_state_root
+      // _public_inputs[1] is new_state_root
+      require(_public_inputs.length >= 2);
+      if (_block_id == 0) {
+         assert(_public_inputs[0] == GENESIS_ROOT);
+      } else {
+         assert(_public_inputs[0] == state_roots[_block_id-1]);         
+      }
+
+      // TODO: state transistion & proof verification
+
+      return true;
    }
 }
