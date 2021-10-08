dc06d622b318734f45a3ef39e3b2d41965b82233
lightsing <light.tsing@gmail.com>
2021-10-08 16:08:34 +0800

    add IVerifier

--- contracts/IVerifier.sol
+++ contracts/IVerifier.sol
@@ -2,14 +2,9 @@
 
 pragma solidity ^0.8.0;
 
-import "./IVerifier.sol";
-
-// TODO: generate by plonkit
-contract KeyedVerifier is IVerifier {
+interface IVerifier {
     function verify_serialized_proof(
         uint256[] memory _public_inputs, 
         uint256[] memory _serialized_proof
-    ) override external view returns (bool) {
-        return true;
-    }
+    ) external view returns (bool);
 }
