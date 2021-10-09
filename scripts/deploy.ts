e05816a48f9c84805043f6aeaa01d55ede6573dc
lightsing <light.tsing@gmail.com>
2021-10-09 15:13:36 +0800

    skip verify on localhost

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,4 +1,5 @@
 import { run, ethers } from "hardhat";
+import * as hre from "hardhat";
 import { default as tokens} from "../tokens";
 
 async function main() {
@@ -8,8 +9,6 @@ async function main() {
   const verifier = await verifierFactory.deploy();
   await verifier.deployed();
   console.log("Verifier deployed to:", verifier.address);
-  
-  
 
   const fluidexFactory = await ethers.getContractFactory("FluiDexDemo");
   const genesisRoot = process.env.GENESIS_ROOT;
@@ -27,21 +26,24 @@ async function main() {
   const fluiDexDelegate = await fluiDexDelegateFactory.deploy(fluiDex.address);
   await fluiDexDelegate.deployed();
   console.log("FluiDexDelegate deployed to:", fluiDexDelegate.address);
-  
-  // await run('verify', {
-  //   address: verifier.address,
-  //   contract: "contracts/Verifier.sol:KeyedVerifier",
-  // });
-  // await run('verify', {
-  //   address: fluiDex.address,
-  //   contract: "",
-  //   constructorArgsParams: [genesisRoot, verifier.address],
-  // });
-  // await run('verify', {
-  //   address: fluiDexDelegate.address,
-  //   contract: "",
-  //   constructorArgsParams: [fluiDex.address],
-  // });
+
+  // skip verify on localhost
+  if (hre.network.name !== "localhost") {
+    await run('verify', {
+      address: verifier.address,
+      contract: "contracts/Verifier.sol:KeyedVerifier",
+    });
+    await run('verify', {
+      address: fluiDex.address,
+      contract: "contracts/FluiDex.sol:FluiDexDemo",
+      constructorArgsParams: [genesisRoot, verifier.address],
+    });
+    await run('verify', {
+      address: fluiDexDelegate.address,
+      contract: "contracts/FluiDexDelegate.sol:FluiDexDelegate",
+      constructorArgsParams: [fluiDex.address],
+    });
+  }
 }
 
 main()
